f3cff1346f63bbe89a7cc40f2b9d082d087ba961
Narek <narek.tarverdyan@bk.ru>
2017-05-08 15:56:13 +0300

    init

--- dev/js/main.js
+++ dev/js/main.js
@@ -7,4 +7,17 @@ $(document).ready(function() {
 		arrows: false,
 		dots: true,
 	});
+	$('.lk__sidebar-tab').click(function(event) {
+		event.preventDefault();
+		$('.lk__sidebar-tab').removeClass('active-tab');
+		$(this).addClass('active-tab');
+		var index = $(this).index();
+		$('.lk__content').removeClass('active');
+		$('.lk__content-wrapper .lk__content').eq(index).addClass('active');
+	});
+	$('.more-order-detail').click(function(event) {
+		event.preventDefault();
+		$('.order__item').removeClass('active');
+		$(this).parents('.order__item').addClass('active');
+	});
 });
